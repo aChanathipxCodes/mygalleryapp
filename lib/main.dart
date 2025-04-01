c2b48b9d41ecf08332cbcc11db03d4e18e4aea84
Chanathip Sukphonsawan <chanathips.work@gmail.com>
2025-04-02 02:27:24 +0700

    Create Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/color.dart';
 import 'package:myapp/gallery_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: primary,
+        appBarTheme: const AppBarTheme(centerTitle: true),
+      ),
+      home: GalleryPage(),
+    );
+  }
+}
