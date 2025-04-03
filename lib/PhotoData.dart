53bfaee165c676db6d37de424f7bcad2224a3157
Chanathip Sukphonsawan <chanathips.work@gmail.com>
2025-04-03 15:00:38 +0700

    Add more Photo

--- lib/PhotoData.dart
+++ lib/PhotoData.dart
@@ -10,4 +10,19 @@ List<Photo> photos = [
   Photo(
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSabzA9oa2zLrYixKuJzdMXHxSErlZINxWuSg&s',
   ),
+  Photo(
+    'https://drive.google.com/uc?export=view&id=1IRPsWsgKw_P4tV8Q2SObd02G-i0zvK-3',
+  ),
+  Photo(
+    'https://drive.google.com/uc?export=view&id=1xytE5pqnWcSw8nRWAiVx2G3OEBfk3L8U',
+  ),
+  Photo(
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGkiLqFFrp76YnmuKGX8CChGtClT2f_x7MeQ&s',
+  ),
+  Photo(
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJu0yTUoOQYwbWtjUOwjZKlYoz8YF_OEIgmA&s',
+  ),
+  Photo(
+    'https://drive.google.com/uc?export=view&id=1BJtv0e1KLPA1CB89TQyb4Rbkd7EO-JNQ',
+  ),
 ];
